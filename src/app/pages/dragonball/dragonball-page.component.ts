591b9ca7b636e7b1f7cb4ad814e88c42110735fd
Jpalominoo <77jesuspg@gmail.com>
2025-04-24 11:33:00 +0200

    Ultima version proyecto de prueba

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -16,15 +16,15 @@ templateUrl: './dragonball-page.component.html',
 export class DragonballPageComponent {
 
 
-        name = signal('Gohan');
-        power = signal(100);
+        name = signal('');
+        power = signal(0);
 
 
     characters = signal<Character[]>([
         { id: 1, name: 'Goku', power: 9001 },
-        { id: 2, name: 'Vegeta', power: 9000 },
+/*         { id: 2, name: 'Vegeta', power: 9000 },
         { id: 3, name: 'Trunks', power: 3500 },
-        { id: 4, name: 'Yamcha', power: 500 },
+        { id: 4, name: 'Yamcha', power: 500 }, */
     ]);
 
 
